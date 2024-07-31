39e8f27b91d88d564350d3d1a96838b12c7eccb6
octuaOSdev <144250548+octuaOSdev@users.noreply.github.com>
2024-07-31 17:14:17 +0200

    Update PingWork.py

--- PingWork.py
+++ PingWork.py
@@ -4,17 +4,14 @@ def ping(hostname):
     param = '-n' if os.sys.platform().lower()=='win32' else '-c'
     PINGresponse = os.system(f"ping {param} 1 {hostname}")
     return PINGresponse
-def POST():
+def POST(version,username,password,url):
     
     # Replace these with your actual username and password
-    username = 'your_username'
-    password = 'your_password'
 
-    url = 'https://login.minecraft.net'
     data = {
         'user': username,
         'password': password,
-        'version': '13'
+        'version': version
     }
 
     response = requests.post(url, data=data)
